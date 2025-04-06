792e6466d043f7317730680e1d1dd01b87e5ac8e
Nihan Cj <nihancj.com@gmail.com>
2025-04-06 11:22:18 +0530

    nvim(blink): disable cmdline

--- .config/nvim/lua/user/plugin-conf/blink.lua
+++ .config/nvim/lua/user/plugin-conf/blink.lua
@@ -40,13 +40,15 @@ appearance = {
   nerd_font_variant = 'mono'
 },
 
+cmdline = { enabled = false },
+
 -- (Default) Only show the documentation popup when manually triggered
 completion = { documentation = { auto_show = false } },
 
 -- Default list of enabled providers defined so that you can extend it
 -- elsewhere in your config, without redefining it, due to `opts_extend`
 sources = {
-  default = { 'lsp', 'path', 'snippets', 'buffer' },
+  default = { 'snippets', 'lsp', 'path', 'buffer' },
 },
 
 -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
