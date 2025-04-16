717caa1d04e22a4ee330f795d2cddfd14c0af14a
Nihan Cj <nihancj.com@gmail.com>
2025-04-16 10:36:36 +0530

    nvim(lsp): new keybinds

--- .config/nvim/lua/user/lsp/init.lua
+++ .config/nvim/lua/user/lsp/init.lua
@@ -5,5 +5,7 @@ require("user.lsp.rust-analyzer")
 vim.diagnostic.config({virtual_text = { current_line = true }})
 
 local nmap = require("user.functions").keymaps.nmap
-nmap({"grl", "<cmd>lua vim.diagnostic.open_float()<CR>"})
+nmap({"gl", "<cmd>lua vim.diagnostic.open_float()<CR>"})
 nmap({"grf", "<cmd>lua vim.lsp.buf.format()<CR>"})
+nmap({"gd", "<cmd>lua vim.lsp.buf.definition()<CR>"})
+nmap({"gD", "<cmd>lua vim.lsp.buf.declaration()<CR>"})
